3067bd33f5a96fcab317d67875c3087e887c51bc
maxkolt <12345kolt@gmail.com>
2025-05-12 01:45:00 +0700

    init

--- src/bot/index.js
+++ src/bot/index.js
@@ -143,6 +143,11 @@ app.get('/', (req, res) => {
   res.send('Bot is running');
 });
 
+// Задать endpoint для health check
+app.get('/healthz', (req, res) => {
+  res.status(200).send('Bot is running');
+});
+
 // Запуск HTTP сервера на порту, который используется Render
 app.listen(PORT, () => {
   console.log(`HTTP сервер запущен на порту ${PORT}`);
