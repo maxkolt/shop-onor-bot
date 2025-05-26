6ac55b2cd8034e2346fe126020aac879de6499b2
maxkolt <12345kolt@gmail.com>
2025-05-27 00:15:23 +0700

    init

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+BOT_TOKEN='7703511185:AAHjt8dRuP4uSPfzRXCgZGmYjek8Zwc0OmE'
+MONGO_URI='mongodb+srv://12345kolt:11223355@test.ytbi01k.mongodb.net/?retryWrites=true&w=majority&appName=test'
+FUNCTION_URL='https://functions.yandexcloud.net/d4e1eje8e7s44fs41uk4'
+
 echo "📦 Архивируем проект (без node_modules)..."
 zip -r bot-function.zip index.js package.json package-lock.json src > /dev/null
 
@@ -15,17 +19,14 @@ yc serverless function version create \
   --memory 256MB \
   --execution-timeout 30s \
   --source-path ./bot-source \
-  --environment BOT_TOKEN='7703511185:AAHjt8dRuP4uSPfzRXCgZGmYjek8Zwc0OmE' \
-  --environment 
-MONGO_URI='mongodb+srv://12345kolt:11223355@test.ytbi01k.mongodb.net/?retryWrites=true&w=majority&appName=test'
+  --environment "BOT_TOKEN=${BOT_TOKEN}" \
+  --environment "MONGO_URI=${MONGO_URI}"
 
 echo "🔗 Обновляем Telegram webhook..."
-curl -s -X POST https://api.telegram.org/bot7703511185:AAHjt8dRuP4uSPfzRXCgZGmYjek8Zwc0OmE/setWebhook 
-\
-  -d "url=https://functions.yandexcloud.net/d4e1eje8e7s44fs41uk4" > /dev/null
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/setWebhook" \
+  -d "url=${FUNCTION_URL}"
 
 echo "📡 Проверяем webhook..."
-curl -s https://api.telegram.org/bot7703511185:AAHjt8dRuP4uSPfzRXCgZGmYjek8Zwc0OmE/getWebhookInfo
+curl -s "https://api.telegram.org/bot${BOT_TOKEN}/getWebhookInfo"
 
-echo "✅ Готово! Код загружен, webhook обновлён, бот готов принимать сообщения."
-
+echo -e "\n✅ Готово! Код загружен, webhook обновлён, бот готов принимать сообщения."
